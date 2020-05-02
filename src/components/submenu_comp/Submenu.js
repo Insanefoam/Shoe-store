966de5bbc3f0b372bfd6b4b22936ab8d46217dcf
Daniil <spartaq123456789@gmail.com>
2020-05-02 19:48:22 +0600

    some submenu commit

--- src/components/submenu_comp/Submenu.js
+++ src/components/submenu_comp/Submenu.js
@@ -27,7 +27,17 @@ export default class Submenu extends Component {
     render() {
         return (
             <div className="submenu" style={this.state.style}>
-                submenu
+                <div className="submenu__item">
+                    <img className="submenu__image" src="./assets/models/converse/chuck_taylor_all_star.png"></img>
+                    <div className="submenu__title">Chuck Taylor All Star</div>
+                </div>
+                <div className="submenu__info">
+                    <img className="submenu__close" src="./assets/close.svg"></img>
+                    <input className="submenu__countinput" value="1 шт."></input>
+                    <div className="submenu__finalprice">2200$</div>
+                    <img className="submenu__cart" src="./assets/cart.svg"></img>
+                    <div className="submenu__info">Супер кроссы класс </div>
+                </div>
             </div>
         )
     }
